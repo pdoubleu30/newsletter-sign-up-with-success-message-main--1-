c9ce4713e541c43e15e8126f43be4d62e4ba5a3b
pdoubleu30 <phwonka@gmail.com>
2025-06-20 12:39:54 -0600

    Update main.js

--- main.js
+++ main.js
@@ -32,9 +32,8 @@ document.getElementById('success').style.display = "none";
 document.getElementById('mainPage').style.display = "block";
 document.getElementById('newsTwo').style.display = "block";
 
-if (window.innerWidth > 670 ) {
+if (window.innerWidth < 670 ) {
   document.getElementById('newsTwo').style.display = "none";
-  document.getElementById('mainPage').style.display = "block";
 }
 
 form.reset();
